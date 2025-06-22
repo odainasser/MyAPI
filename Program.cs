69a1d247a4a6e78100aee9582512543f340d353f
Odai Nasser <karajah90@icloud.com>
2025-06-22 15:57:15 +0400

    adding EF

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyAPI.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
